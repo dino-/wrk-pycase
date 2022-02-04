287e4e99c62bfac5c0992d367fb41936276ac903
Dino Morelli <dino@ui3.info>
2022-02-04 00:01:58 -0500

    Using lower case for all generic type variables
    This is more like Haskell.
    
    Also reformatted the code and added more comments.

--- lib/Control/Case.py
+++ lib/Control/Case.py
@@ -1,20 +1,32 @@
-from typing import Callable, Generic, TypeVar
+from typing import Callable, TypeVar
 
 
-A = TypeVar('A')
-B = TypeVar('B')
+a = TypeVar('a')
+b = TypeVar('b')
 
 
 # FIXME Should live in Data.Function
-def const(b: B) -> Callable[[A], B]:
-  return lambda _: b
+def const(y: b) -> Callable[[a], b]:
+  return lambda _: y
 
-Case = tuple[Callable[[A], bool], Callable[[A], B]]
-Cases = list[Case[A, B]]
 
-def case(val: A, cases: Cases[A, B]) -> B:
-  matchedCaseFunc = (list(filter(lambda t: t[0](val), cases))[0])[1]
+Case = tuple[Callable[[a], bool], Callable[[a], b]]
+Cases = list[Case[a, b]]
+
+
+def case(val: a, cases: Cases[a, b]) -> b:
+  # Filter the cases for the ones whose predicate matches given the value and
+  # take the first one
+  matchedCase: Case[a, b] = list(filter(lambda t: t[0](val), cases))[0]
+
+  # Isolate the function to be performed (the right side of the tuple, or
+  # element 1)
+  matchedCaseFunc: Callable[[a], b] = matchedCase[1]
+
+  # Call it on the value and return the result
   return matchedCaseFunc(val)
 
 
-otherwise: Callable[[A], bool] = const(True)
+# A predicate that always succeeds regardless of the input, for the
+# fall-through case
+otherwise: Callable[[a], bool] = const(True)
