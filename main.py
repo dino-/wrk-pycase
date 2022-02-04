ad59c09de61cc0d18dc5120a6c1f5d84a3a5edfb
Dino Morelli <dino@ui3.info>
2022-02-04 00:28:57 -0500

    Added some examples of using the if statement

--- main.py
+++ main.py
@@ -9,11 +9,32 @@ from Control.Case import case, const, otherwise
 
 
 def main() -> None:
-  output: str = case(int(sys.argv[1]),
+  argAsInt = int(sys.argv[1])
+
+  # With the case expression code
+  output1: str = case(argAsInt,
     [ (lambda n: n < 10, const('Less than 10'))
     , (otherwise       , const('Not less than 10'))
     ])
-  print(output)
+  print(output1)
+
+  # With a conventional if statement. Sad, can't avoid mutation and repetition.
+  # Also awkwardly can't declare the type more than once.
+  if argAsInt < 10:
+    output2: str = 'Less than 10'
+  else:
+    output2 = 'Not less than 10'
+  print(output2)
+
+  # Could wrap the if in a function so the value can be returned as though it's
+  # an expression
+  def checkIt(n: int) -> str:
+    if argAsInt < 10:
+      return 'Less than 10'
+    else:
+      return 'Not less than 10'
+  output3: str = checkIt(int(sys.argv[1]))
+  print(output3)
 
 
 if __name__ == '__main__': main()
